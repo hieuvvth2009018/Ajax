5de7f0c0664e56d524e5a07e5de59078a4b48783
hieuvvth2009018 <73455351+hieuvvth2009018@users.noreply.github.com>
2021-08-25 23:44:43 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get('/Ajax',[\App\Models\Ajax::class,'display']);
+Route::post('/Ajax',[\App\Models\Ajax::class,'save']);
